59c3c7c7a73ae46652520da7cc50c2bde2cf09c7
Sumith Deepan <sumith.d@falainacloud.com>
2021-12-14 16:45:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import { useState } from 'react';
-import List from './List'
-import data from './data'
+import { useState } from "react";
+import List from "./List";
+import data from "./data";
 
 function App() {
-  const [people,setPeople]=useState(data)
-    return <main>
+  const [people, setPeople] = useState(data);
+  return (
+    <main>
       <section className="container">
-        <h3>{people.length} Birthday Today</h3>
-        <List people={people} />
-        <button onClick={()=>{setPeople([])}} >Clear All</button>
-      </section>
-    </main>;
+        <h3>
+          {" "}
+          {people.length}
+          Birthday Today{" "}
+        </h3>{" "}
+        <List people={people} />{" "}
+        <button
+          onClick={() => {
+            setPeople([]);
+          }}
+        >
+          {" "}
+          Clear All{" "}
+        </button>{" "}
+      </section>{" "}
+    </main>
+  );
 }
 
 export default App;
